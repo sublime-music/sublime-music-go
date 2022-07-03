cb69f470c365df01f6b7c61cc025ac71c4baf13d
Sumner Evans <me@sumnerevans.com>
2022-07-02 21:26:04 -0600

    more playlist UI stuff
    Signed-off-by: Sumner Evans <me@sumnerevans.com>

--- ui/playlists.go
+++ ui/playlists.go
@@ -11,9 +11,15 @@ type PlaylistsTab struct {
 
 func CreatePlaylistsTab() *PlaylistsTab {
 	builder := gtk.NewBuilderFromResource("/app/sublimemusic/SublimeMusicNext/ui/playlists.ui")
-	playlistsTab := &PlaylistsTab{
+
+	playlistScrollWindow := builder.GetObject("playlist-details-content").Cast().(*gtk.ScrolledWindow)
+	playlistDetailsFlap := builder.GetObject("playlist-details-flap").Cast().(*adw.Flap)
+	vadjustment := playlistScrollWindow.VAdjustment()
+	vadjustment.ConnectValueChanged(func() {
+		playlistDetailsFlap.SetRevealFlap(vadjustment.Value() > 70)
+	})
+
+	return &PlaylistsTab{
 		Leaflet: builder.GetObject("root").Cast().(*adw.Leaflet),
 	}
-
-	return playlistsTab
 }
