fdb12dda5f7dd6cf83af78ca791c425060c48124
Sumner Evans <me@sumnerevans.com>
2022-07-04 01:17:43 -0600

    WIP smallest?
    Signed-off-by: Sumner Evans <me@sumnerevans.com>

--- ui/playercontrols.go
+++ ui/playercontrols.go
@@ -12,12 +12,14 @@ type PlayerControls struct {
 func CreatePlayerControls() *PlayerControls {
 	builderBig := gtk.NewBuilderFromResource("/app/sublimemusic/SublimeMusicNext/ui/playercontrols.ui")
 	builderSmall := gtk.NewBuilderFromResource("/app/sublimemusic/SublimeMusicNext/ui/playercontrolssmall.ui")
+	builderSmallest := gtk.NewBuilderFromResource("/app/sublimemusic/SublimeMusicNext/ui/playercontrolssmallest.ui")
 
 	squeezer := adw.NewSqueezer()
 	squeezer.Add(builderBig.GetObject("root").Cast().(*gtk.ActionBar))
 	squeezer.Add(builderSmall.GetObject("root").Cast().(*gtk.Box))
+	squeezer.Add(builderSmallest.GetObject("root").Cast().(*gtk.Box))
 	squeezer.SetHomogeneous(false)
-	
+
 	pc := &PlayerControls{
 		Squeezer: squeezer,
 	}
